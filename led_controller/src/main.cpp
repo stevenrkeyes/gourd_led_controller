0bc560ba6e823e96468937ba20fd895581faac32
djbhindi <djbhindi@gmail.com>
2025-07-15 12:01:10 -0700

    refactor

--- led_controller/src/main.cpp
+++ led_controller/src/main.cpp
@@ -1,89 +1,36 @@
-#include <OctoWS2811.h>
-#include <Bounce2.h>
-#include <FastLED.h>
+#include <Arduino.h>
+#include "led_strips.h"
+#include "eyes.h"
+#include "buttons.h"
 
 #define LOOP_DELAY_MS 2
+// Do we need these?
 #define LED_CHIPSET WS2811
 #define LED_COLOR_ORDER WGRB
 
 constexpr int BOARD_LED_PIN = 13;
 
-constexpr int BUTTON_COLUMN_1_PIN = 0;
-constexpr int BUTTON_ROW_1_PIN = 1;
-
-constexpr int LED_STRIP_1_PIN = 2;
-constexpr int LED_STRIP_NUM_LEDS = 15;
-
-constexpr int EYE_LEDS_1_PIN = 22;  // Try a different pin
-constexpr int NUM_EYES = 3;  // Try with just 1 eye first
-constexpr int OUTER_EYE_NUM_LEDS = 24;
-constexpr int INNER_EYE_NUM_LEDS = 8;
-constexpr int EYE_NUM_LEDS = OUTER_EYE_NUM_LEDS + INNER_EYE_NUM_LEDS;
-
-// OctoWS2811 for main LED strip
-DMAMEM int displayMemoryLeds[LED_STRIP_NUM_LEDS];
-int drawingMemoryLeds[LED_STRIP_NUM_LEDS];
-const int config = WS2811_RGBW | WS2811_800kHz;
-OctoWS2811 leds(LED_STRIP_NUM_LEDS, displayMemoryLeds, drawingMemoryLeds, config, LED_STRIP_1_PIN);
-
-// FastLED for eye LEDs
-CRGB eyeLeds[NUM_EYES * EYE_NUM_LEDS];
-
-Bounce button1(BUTTON_COLUMN_1_PIN, 50);
-
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting");
-  
   pinMode(BOARD_LED_PIN, OUTPUT);
-  pinMode(BUTTON_COLUMN_1_PIN, INPUT_PULLUP);
 
-  leds.begin();
-  leds.show(); // Clear LEDs to off
+  setupLedStrips();
+  setupEyes();
+  setupButtons();
 
-  // Set all LEDs to white using the white channel
-  for (int i = 0; i < LED_STRIP_NUM_LEDS; i++) {
-    leds.setPixel(i, 0x00AAAA00); // W=0, G=170, R=0, B=0
-  }
-  leds.show();
-
-  // Initialize FastLED for eye LEDs
-  FastLED.addLeds<WS2811, EYE_LEDS_1_PIN, GRB>(eyeLeds, NUM_EYES * EYE_NUM_LEDS);
-  FastLED.clear();
-  FastLED.show();
-
-  // Set eye LED pattern
-  for (int i = 0; i < NUM_EYES; i++) {
-    int eye_offset = i * EYE_NUM_LEDS;
-    int inner_eye_offset = eye_offset + OUTER_EYE_NUM_LEDS;
-    for (int j = 0; j < OUTER_EYE_NUM_LEDS; j++) {
-      eyeLeds[eye_offset + j] = CRGB::Red; // Red outer ring
-    }
-    for (int j = 0; j < INNER_EYE_NUM_LEDS; j++) {
-      eyeLeds[inner_eye_offset + j] = CRGB::Red; // Green inner ring
-    }
-  }
-  FastLED.show();
   Serial.println("Setup complete");
 }
 
 void loop() {
-  // Simple test - just flash the LED
+  // flash LED to confirm the program is running.
   digitalWrite(BOARD_LED_PIN, HIGH);
   delay(500);
   digitalWrite(BOARD_LED_PIN, LOW);
   delay(500);
 
-  // Set eye LED pattern
-  for (int i = 0; i < NUM_EYES; i++) {
-    int eye_offset = i * EYE_NUM_LEDS;
-    int inner_eye_offset = eye_offset + OUTER_EYE_NUM_LEDS;
-    for (int j = 0; j < OUTER_EYE_NUM_LEDS; j++) {
-      eyeLeds[eye_offset + j] = CRGB::Red; // Red outer ring
-    }
-    for (int j = 0; j < INNER_EYE_NUM_LEDS; j++) {
-      eyeLeds[inner_eye_offset + j] = CRGB::Red; // Green inner ring
-    }
-  }
-  FastLED.show();
+  loopLedStrips();
+  loopEyes();
+  loopButtons();
+  delay(LOOP_DELAY_MS);
 }
